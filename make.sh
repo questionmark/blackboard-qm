450b5480c97c67cbea58a6aab8ba87aaf14b06db
Sahil Sethi <sahil.sethi@questionmark.co.uk>
2009-10-19 13:54:40 +0000

    [#232] WebRoot folder removed.
    git-svn-id: http://projects.oscelot.org/svn/blackboard-qm/branches/qbc-8@22 5dbac1ef-6dec-4765-9204-c67e81cc76ec

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ./classpath.sh
-mkdir -p WebRoot/WEB-INF/classes
-find WebRoot -name "*.java" -exec javac -target 1.5 -d WebRoot/WEB-INF/classes {} +
+mkdir -p WebContent/WEB-INF/classes
+find src -name "*.java" -exec javac -target 1.5 -d WebContent/WEB-INF/classes {} +
